49422709d6850e57ebcb041541067ccf9cb4db19
Andrew Smith <848smith@gmail.com>
2020-03-03 02:37:59 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,7 +13,9 @@ class Scraper
       course = Course.new
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
-      course.description = post.css("p").te
+      course.description = post.css("p").text
+    end
+    binding.pry
   end
   
   def print_courses
